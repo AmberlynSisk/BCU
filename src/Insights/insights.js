351ed553e343cbc8be676d1ee4f5ad071c29aeb3
Riley Sanchez <95667016+RileySanchez@users.noreply.github.com>
2022-02-25 18:09:59 -0700

    Riley Insights Page

---
+++ src/Insights/insights.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { PieChart, Pie } from "recharts";
+
+const App = () => {
+  const data = [
+    { name: "Income", students: 400 },
+    { name: "Entertainment", students: 700 },
+    { name: "Housing", students: 200 },
+    { name: "Utilities", students: 1000 },
+    { name: "Vehicles", students: 1000 },
+    { name: "Food", students: 1000 },
+    { name: "Gas", students: 1000 },
+    { name: "Misc.", students: 1000 },
+  ];
+
+  return (
+    <PieChart width={500} height={500}>
+      <Pie data={data} dataKey="students" outerRadius={250} fill="green" />
+    </PieChart>
+  );
+};
+
+export default App;
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Insights from "../Insights/insights";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <h2>Financial Insights for:</h2>
+
+        <Insights />
       </div>
     );
   }
